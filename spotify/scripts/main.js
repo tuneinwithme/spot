4b4b0b8e129c22d52fc4c706642cb27100dd3bd4
Sean Zhu <interestinglythere@gmail.com>
2014-01-25 06:28:52 -0800

    added room change and live updates for web

--- spotify/scripts/main.js
+++ spotify/scripts/main.js
@@ -1,18 +1,30 @@
 require([
   '$api/models',
-  'scripts/language-example',
-  'scripts/cover-example',
-  'scripts/button-example',
-  'scripts/playlist-example',
   'scripts/livedj'
-], function(models, languageExample, coverExample, buttonExample, playlistExample, livedj) {
+], function(models, livedj) {
   'use strict';
 
-  // languageExample.doHelloWorld();
-  // coverExample.doCoverForAlbum();
-  // buttonExample.doShareButtonForArtist();
-  // buttonExample.doPlayButtonForAlbum();
-  // playlistExample.doPlaylistForAlbum();
   livedj.init();
 
 });
+
+
+
+// require([
+//   '$api/models',
+//   'scripts/language-example',
+//   'scripts/cover-example',
+//   'scripts/button-example',
+//   'scripts/playlist-example',
+//   'scripts/livedj'
+// ], function(models, languageExample, coverExample, buttonExample, playlistExample, livedj) {
+//   'use strict';
+
+//   // languageExample.doHelloWorld();
+//   // coverExample.doCoverForAlbum();
+//   // buttonExample.doShareButtonForArtist();
+//   // buttonExample.doPlayButtonForAlbum();
+//   // playlistExample.doPlaylistForAlbum();
+//   livedj.init();
+
+// });
